c1388a8c185c18cc3ab4599697248d655e27f728
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2021-12-06 22:38:58 +0900

    update

--- setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-wget -O - https://github.com/todo-group/standards/archive/develop.tar.gz | tar zxf - standards-develop/config && mv standards-develop/config/* config/ && rm -rf standards-develop
