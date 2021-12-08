b1c56a28fca97c7acd39fef8804fe5d468de259a
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2021-12-08 09:41:09 +0900

    update

--- cmake/bcl.cmake
+++
@@ -1,9 +0,0 @@
-option(BCL_INSTALL OFF)
-option(BCL_BUILD_TESTS OFF)
-option(BCL_BUILD_EXAMPLES OFF)
-
-include(FetchContent)
-FetchContent_Declare(
-  bcl
-  GIT_REPOSITORY https://github.com/cmsi/bcl.git
-)
